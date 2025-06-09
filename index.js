42d9edf1a80725c03fcd4fa2203c4ab7efe249f0
miaa-isabella <miaisa1x@gmail.com>
2025-06-08 21:31:02 -0600

    tengo que conectar la api

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import fs from "fs";
 import bodyParser from "body-parser";
-
+import cors from "cors"; 
 
 
     const app = express();
     app.use(bodyParser.json());
+    app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 
     const readData = ()=> {try{
     const data = fs.readFileSync("./db.json");
@@ -14,17 +21,19 @@ import bodyParser from "body-parser";
     console.log(error);
     }
     };
-
+    
     readData();   
 
     const writeData = (data) =>{try{
-    fs.writeFileSync("./db.json", JSON.stringfy(data));
+
+    fs.writeFileSync("./db.json", JSON.stringify(data));
+
     } catch(error){
     console.log(error);
     }
 
     }
-
+    
     app.get("/peliculas",(req, res) => 
     {
         const data = readData();
@@ -43,19 +52,46 @@ import bodyParser from "body-parser";
     });
 
     app.post("/peliculas",(req, res) => {
- const data = readData();
- const body = req.body;
- const nuevapeli = {
-    id: data.peliculas.length + 1,...body
- };
- data.peliculas.push(nuevapeli);
- writeData(data);
- res.json(nuevapeli);
+   
+
+      const data = readData();
+      const body = req.body;
+      const nuevapeli = {
+      id: data.peliculas.length + 1,...body, 
+      };
+
+      data.peliculas.push(nuevapeli);
+      writeData(data);
+      res.json(nuevapeli);
     });
 
+    app.put("/peliculas/:id", (req, res) => {
+        const data = readData();
+         const body = req.body;
+         const id = parseInt(req.params.id);
+         const peliculaIndex = data.peliculas.findIndex((pelicula) => pelicula.id === id);
+         data.peliculas[peliculaIndex] = {
+            ...data.peliculas[peliculaIndex],
+            ...body,
+         };
+         writeData(data);
+         res.json({message: "pelicula actualizada"});
+    });
 
+    app.delete("/peliculas/:id", (req, res) => {
+       const data = readData();
+         const id = parseInt(req.params.id);
+         const peliculaIndex = data.peliculas.findIndex((pelicula) => pelicula.id === id);
+      data.peliculas.splice(peliculaIndex, 1);
+      writeData(data);
+      res.json({message: "dato eliminado "});
 
+    });
 
     app.listen(3000,() => {
     console.log('server listening on port 3000');
     });
+ 
+
+
+
