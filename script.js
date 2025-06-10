91c75e716fdeb9188788499be076832c712de87a
miaa-isabella <miaisa1x@gmail.com>
2025-06-09 22:37:28 -0600

    5to avance

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ fetch('http://localhost:3000/peliculas')
 .then(res => res.json())
 .then(data => console.log(data))
 .catch(error => console.log(error))
+
+const mostrarData = (data) => {
+    let body =''
+    for(let i = 0; i < data.length; i++){
+        body += '<tr><td>${data[i].name}</td></tr><td>${data[i].productor}</td><td>${data[i].anio}</td></tr>'
+    }
+    document.getElementById('data').innerHTML = body
+}
